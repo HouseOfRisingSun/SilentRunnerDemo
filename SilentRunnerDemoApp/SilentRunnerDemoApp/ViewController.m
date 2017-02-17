9086d01623085708f7869fa0c7fe235b73f6c374
Andriy Batutin <andriy.batutin@email.com>
2017-02-17 11:52:41 +0200

    up tp 0.0.4 v

--- SilentRunnerDemoApp/SilentRunnerDemoApp/ViewController.m
+++ SilentRunnerDemoApp/SilentRunnerDemoApp/ViewController.m
@@ -18,16 +18,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [SRClientPool addClient:[UIApplication sharedApplication].delegate forTag:@"app"];
-    self.serv = [SRServer serverWithURL:@"ws://localhost:9000/chat" withMessageHandler:^(NSString * msg) {
-        NSError* error = nil;
-        SRCommand* command = (SRCommand*)[SRMessageHandler createCommandFromMessage:msg withError:^(NSError* error){
-            [self.serv sendErrorMessage:error];
-        }];
-        [SRCommandHandler runCommand:command withError:&error];
-        if ( error ){
-            [self.serv sendErrorMessage:error];
-        }
-    } withErrorHandler:^(NSError * error) {
+    self.serv = [SRServer serverWithURL:@"ws://localhost:9000/chat" withErrorHandler:^(NSError * error) {
         [self.serv sendErrorMessage:error];
     }];
     [self.serv runServer];
